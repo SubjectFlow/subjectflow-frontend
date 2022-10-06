164cad351dcf3f0eee8417b821c2e61029b87f84
crispycaterpillar <37645482+LukeRusso@users.noreply.github.com>
2022-10-06 23:59:59 +1100

    created arrow head

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -4,8 +4,10 @@ import { Point } from "../Types";
 
 const xCurveFactor = 0.2;
 const yCurveFactor = 0.06;
-const startCurveFactor = 1;
-const endCurveFactor = 1;
+const curveFactor1 = 1;
+const curveFactor2 = 1;
+const arrowHeadHeight = 12;
+const arrowHeadLength = 12;
 
 type ArrowProps = {
   start: Point;
@@ -13,53 +15,68 @@ type ArrowProps = {
 };
 
 function Arrow(props: ArrowProps) {
-  let bottom, left;
+  let bottom, left, xHeadDiff;
+
   const diff: Point = {
     x: Math.abs(props.end.x - props.start.x),
     y: Math.abs(props.end.y - props.start.y),
   };
 
-  let svgStart: Point = {
+  let svgLineStart: Point = {
     x: 0,
     y: 0,
   };
 
-  let svgEnd: Point = JSON.parse(JSON.stringify(diff));
+  let svgLineEnd: Point = {
+    x: diff.x - arrowHeadLength,
+    y: diff.y,
+  };
 
   if (props.start.x > props.end.x) {
-    svgStart.x = diff.x;
-    svgEnd.x = 0;
+    svgLineStart.x = diff.x;
+    svgLineEnd.x = arrowHeadLength;
     left = props.end.x;
-  } else left = props.start.x;
+    xHeadDiff = -1 * arrowHeadLength;
+  } else {
+    left = props.start.x;
+    xHeadDiff = arrowHeadLength;
+  }
 
   if (props.start.y <= props.end.y) {
-    svgStart.y = diff.y;
-    svgEnd.y = 0;
+    svgLineStart.y = diff.y + arrowHeadHeight / 2;
+    svgLineEnd.y = 0;
     bottom = props.start.y;
   } else bottom = props.end.y;
 
-  const svgDiff: Point = {
-    x: svgEnd.x - svgStart.x,
-    y: svgEnd.y - svgStart.y,
+  const svgLineDiff: Point = {
+    x: svgLineEnd.x - svgLineStart.x,
+    y: svgLineEnd.y - svgLineStart.y,
   };
 
   return (
     <svg
       className="arrow"
       width={diff.x}
-      height={diff.y}
+      height={diff.y + arrowHeadHeight / 2}
       xmlns="http://www.w3.org/2000/svg"
       style={{ bottom: bottom, left: left }}
     >
       <path
         className="arrow__line"
         d={
-          `M ${svgStart.x} ${svgStart.y} ` +
-          `C ${svgStart.x + startCurveFactor * xCurveFactor * svgDiff.x} ` +
-          `${svgStart.y + startCurveFactor * yCurveFactor * svgDiff.y}, ` +
-          `${svgEnd.x - endCurveFactor * xCurveFactor * svgDiff.x} ` +
-          `${svgEnd.y - endCurveFactor * yCurveFactor * svgDiff.y}, ` +
-          `${svgEnd.x} ${svgEnd.y}`
+          `M ${svgLineStart.x} ${svgLineStart.y} ` +
+          `C ${svgLineStart.x + curveFactor1 * xCurveFactor * svgLineDiff.x} ` +
+          `${svgLineStart.y + curveFactor1 * yCurveFactor * svgLineDiff.y}, ` +
+          `${svgLineEnd.x - curveFactor2 * xCurveFactor * svgLineDiff.x} ` +
+          `${svgLineEnd.y - curveFactor2 * yCurveFactor * svgLineDiff.y}, ` +
+          `${svgLineEnd.x} ${svgLineEnd.y}`
+        }
+      />
+      <polygon
+        points={
+          `${svgLineEnd.x + xHeadDiff},${svgLineEnd.y} ` +
+          `${svgLineEnd.x},${svgLineEnd.y + arrowHeadHeight / 2} ` +
+          `${svgLineEnd.x},${svgLineEnd.y - arrowHeadHeight / 2}`
         }
       />
     </svg>
