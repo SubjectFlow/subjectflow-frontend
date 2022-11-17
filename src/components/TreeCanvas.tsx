6906b8abe8a9f647fb7b9e7dcce8a6562b077d4a
crispycaterpillar <crispy.caterpillar@gmail.com>
2022-11-17 13:53:03 +1100

    raised adjList state to TreeCanvas level

--- src/components/TreeCanvas.tsx
+++ src/components/TreeCanvas.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { Tree } from "./Tree";
 import { Point } from "../utils/Point";
 import "../styles/Canvas.css";
+import { useNodeSystem } from "../hooks/useNodeSystem";
 
-function Canvas() {
+function TreeCanvas() {
   const [dragging, setDragging] = useState(false);
   const [disp, setDisp] = useState(new Point(0, 0));
   const [position, setPosition] = useState(new Point(0, 0));
+  const { loaded, adjList } = useNodeSystem();
 
   const onMouseDown = (e: React.MouseEvent) => {
     if (e.button !== 0) return;
@@ -36,9 +38,9 @@ function Canvas() {
       onMouseMove={(e) => onMouseMove(e)}
       onMouseUp={(e) => onMouseUp(e)}
     >
-      <Tree disp={disp} />
+      {loaded && <Tree disp={disp} adjList={adjList} />}
     </div>
   );
 }
 
-export default Canvas;
+export default TreeCanvas;
