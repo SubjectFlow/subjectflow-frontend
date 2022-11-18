4ee72f646dd969368b4ed7f9abdf5379687596b0
crispycaterpillar <crispy.caterpillar@gmail.com>
2022-11-18 12:34:30 +1100

    smoothed search click interpolation

--- src/components/TreeCanvas.tsx
+++ src/components/TreeCanvas.tsx
@@ -4,16 +4,23 @@ import { Point } from "../utils/Point";
 import "../styles/TreeCanvas.css";
 import { useNodeSystem } from "../hooks/useNodeSystem";
 import { SearchBar } from "./SearchBar";
+import { useInterval } from "../hooks/useInterval";
 
 function TreeCanvas() {
   const [dragging, setDragging] = useState(false);
   const [disp, setDisp] = useState(new Point(0, 0));
-  const [position, setPosition] = useState(new Point(0, 0));
+  const [mousePosition, setMousePosition] = useState(new Point(0, 0));
+  const [targetDir, setTargetDir] = useState<Point | null>(null);
+  const [targetSteps, setTargetSteps] = useState(0);
+  const [originalDisp, setOriginalDisp] = useState(new Point(0, 0));
+
   const { loaded, adjList } = useNodeSystem();
+  const TARGET_LERP_DELAY = 0.02;
+  const TARGET_LERP_STEPS = 40;
 
   const onMouseDown = (e: React.MouseEvent) => {
     if (e.button !== 0) return;
-    setPosition(new Point(e.clientX, e.clientY));
+    setMousePosition(new Point(e.clientX, e.clientY));
     setDragging(true);
   };
 
@@ -21,21 +28,41 @@ function TreeCanvas() {
     if (!dragging) return;
     setDisp((prevDisp) => {
       return new Point(
-        prevDisp.x + e.clientX - position.x,
-        prevDisp.y - e.clientY + position.y
+        prevDisp.x + e.clientX - mousePosition.x,
+        prevDisp.y - e.clientY + mousePosition.y
       );
     });
-    setPosition(new Point(e.clientX, e.clientY));
+    setMousePosition(new Point(e.clientX, e.clientY));
   };
+
+  useInterval(() => {
+    if (targetSteps > 0) {
+      setDisp((prevDisp) =>
+        targetDir
+          ? originalDisp.add(
+              targetDir.mult(
+                Math.cos((targetSteps * Math.PI) / TARGET_LERP_STEPS) / 2 + 0.5
+              )
+            )
+          : prevDisp
+      );
+      setTargetSteps((prev) => prev - 1);
+    }
+  }, TARGET_LERP_DELAY * 1000);
 
   const onMouseUp = (e: React.MouseEvent) => {
     setDragging(false);
   };
 
-  const onSearchClick = (position: Point) => {
-    setDisp(
-      position.flip().add(new Point(window.innerWidth / 2, window.innerHeight / 2))
+  const onSearchClick = (pos: Point) => {
+    setTargetDir(
+      pos
+        .flip()
+        .add(new Point(window.innerWidth / 2, window.innerHeight / 2))
+        .minus(disp)
     );
+    setOriginalDisp(disp);
+    setTargetSteps(TARGET_LERP_STEPS);
   };
 
   return (
